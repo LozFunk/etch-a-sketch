b63e245fb7980c68d9c25e3e8cb6cea3fdb7c527
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-05-19 17:10:19 +0200

    Finished the Etch a Sketch Project Added opacity on trail function

--- code.js
+++ code.js
@@ -1,8 +1,9 @@
 const container = document.querySelector("#container")
+const button = document.querySelector("button")
 
-
-function grid(size){  
-    const gridSize = 680/ size
+function grid(size){
+    container.innerHTML = ''
+    const gridSize = 960/ size
 
     for (let i = 0; i < size * size; i++){
         const grid = document.createElement("div")
@@ -18,21 +19,23 @@ function trail(){
     const grid = document.querySelectorAll(".grid")
         for (const box of grid)
     box.addEventListener("mouseover", function(){
-    box.classList.add("selected")
-    // box.style.backgroundColor = "black";
+    box.classList.add("selected");
+    box.style.backgroundColor = "black";
+    let currentOpacity = parseFloat(box.style.opacity) || 0;
+    if (currentOpacity < 1) {
+        box.style.opacity = (currentOpacity + 0.1).toString();
+    }
     });
-    
-}
-function remove(){
-    container.innerHTML = ''
 }
 
-const button = document.querySelector("button")
 button.addEventListener("click", function(){
-    remove()
-    let size = window.prompt("What size do you want your grid to be?")
-    grid(size)
-    trail()
+    let size = window.prompt("What size do you want your grid to be? (Choose between 1 and 100)")
+    if (size > 1 && size <= 100){
+        grid(size)
+        trail()
+    }else{
+        window.alert("Please enter a valid number between 1 and 100")
+    }   
 })
 
 grid(16)
