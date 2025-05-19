4c376e45ecdccd77a389567bbecaeba492f37856
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-05-19 16:19:14 +0200

    Finshed step 3 and 4 from the Odin Project

--- code.js
+++ code.js
@@ -1,16 +1,39 @@
 const container = document.querySelector("#container")
 
 
-function grid(row, column){   
-    for (let i = 0; i < row * column; i++){
+function grid(size){  
+    const gridSize = 680/ size
+
+    for (let i = 0; i < size * size; i++){
         const grid = document.createElement("div")
-        container.appendChild(grid)
+        grid.style.width =`${gridSize}px`;
+        grid.style.height =`${gridSize}px`;
         grid.classList.add("grid")
+        container.appendChild(grid)
     }  
 };
 
 
+function trail(){
+    const grid = document.querySelectorAll(".grid")
+        for (const box of grid)
+    box.addEventListener("mouseover", function(){
+    box.classList.add("selected")
+    // box.style.backgroundColor = "black";
+    });
+    
+}
+function remove(){
+    container.innerHTML = ''
+}
 
+const button = document.querySelector("button")
+button.addEventListener("click", function(){
+    remove()
+    let size = window.prompt("What size do you want your grid to be?")
+    grid(size)
+    trail()
+})
 
-
-grid(16, 16)
+grid(16)
+trail()
